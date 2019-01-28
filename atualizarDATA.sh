9724cf69578c4957a2ab0134cbb6ffe8ab1f3fd1
Wendley S. Silva <wendley@ufc.br>
2019-01-28 14:42:52 -0300

    first commit

---
+++ atualizarDATA.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Shell script para enviar atualizacoes locais para GitHub
+# Wendley S. Silva – wendley@gmail.com - Jan/2019
+
+NOW=$(date +"%d/%m/%Y") ;
+
+# cd ~/Dropbox/Documentos/PhD/Codigos/;
+#
+# cp gr-lqe/python/getRSSI.py sdr/ ;
+# cp gr-lqe/grc/lqe_getRSSI.xml sdr/ ;
+# cp gr-lqe/python/powerControl.py sdr/ ;
+#
+# cd ~/Dropbox/Documentos/PhD/Codigos/sdr/;
+git add . ;
+git commit -m "atualizacao de $NOW" ;
+git push;
+
+# sleep 1
+echo "\n ... \n Encerrando script \n ..."
+sleep 0.3
